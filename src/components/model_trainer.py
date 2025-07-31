3d6c40919cef9adb355ec57cb10f8a60b8e894fc
SunilKuBehera <imsunil050@gmail.com>
2025-08-01 00:42:56 +0530

    Hyperparameter tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -5,7 +5,6 @@ from dataclasses import dataclass
 from catboost import CatBoostRegressor # type: ignore
 from sklearn.ensemble import (RandomForestRegressor,AdaBoostRegressor,GradientBoostingRegressor)
 from sklearn.linear_model import Lasso, LinearRegression, Ridge
-from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from xgboost import XGBRegressor
@@ -44,7 +43,53 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
                 "Gradient Boosting Regressor": GradientBoostingRegressor()
             }
-            model_report:dict = evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+
+            
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting Regressor": {
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    # 'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "Lasso": {},
+                "Ridge": {},
+                "K-Neighbors Regressor": {
+                    'n_neighbors':[5,7,9,11],
+                    # 'weights':['uniform','distance'],
+                    # 'algorithm':['ball_tree','kd_tree','brute']
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    # 'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            model_report:dict = evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             # Get best model score from dictionary
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 import sys
 import os
 import numpy as np
@@ -5,6 +6,7 @@ import pandas as pd
 import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,11 +19,16 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
-        report = {}
+        report = {}     
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3,n_jobs=-1,verbose=0,refit=True)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
