ea3aae86a27c9c94f51f95ed5ffd9035c1755c7b
SunilKuBehera <imsunil050@gmail.com>
2025-08-04 17:47:01 +0530

    Interface design

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
-from unittest import result
-from flask import Flask, request, render_template
-import numpy as np
+import sys
+import os
 import pandas as pd
-from flask import jsonify
+import webbrowser
+from flask import Flask, request, render_template
 
-from sklearn.preprocessing import StandardScaler
+from src.exception import CustomException
+from src.utils import load_object
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-application = Flask(__name__)
-app = application
+# Tell Flask where to find the templates
+app = Flask(__name__, template_folder='templates')
 
+# --- Routes ---
 @app.route('/')
 def home():
+    # This route will render your home.html file
     return render_template('home.html', results=None)
 
 @app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
-        return render_template('home.html', results=None)
+        return render_template('home.html')
     else:
         try:
             data = CustomData(
@@ -30,16 +33,23 @@ def predict_datapoint():
                 writing_score=float(request.form.get('writing_score'))
             )
 
-            pred_df = data.get_data_as_dataframe()
-            print("Input DataFrame:", pred_df)
+            pred_df = data.get_data_as_data_frame()
+            print("Input DataFrame:\n", pred_df)
 
             predict_pipeline = PredictPipeline()
             results = predict_pipeline.predict(pred_df)
 
-            return render_template('home.html', results=results[0])
+            return render_template('home.html', results=round(results[0], 2))
 
         except Exception as e:
-            return render_template('home.html', results=f"Error occurred: {str(e)}")
+            return f"An error occurred: {e}"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # The URL for your Flask app
+    url = "http://127.0.0.1:5000/"
+
+    # Open the URL in a new browser tab
+    webbrowser.open_new_tab(url)
+
+    # Run the Flask app
     app.run(host="0.0.0.0", debug=True)
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -18,12 +18,11 @@ class PredictPipeline:
             preprocessor=load_object(file_path=preprocessor_path)
             print("After Loading")
             data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
+            preds = model.predict(data_scaled)
             return preds
         
         except Exception as e:
             raise CustomException(e,sys)
-
 
 
 class CustomData:
@@ -37,29 +36,23 @@ class CustomData:
         writing_score: int):
 
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
                 "gender": [self.gender],
-                "race_ethnicity": [self.race_ethnicity],
-                "parental_level_of_education": [self.parental_level_of_education],
+                "race/ethnicity": [self.race_ethnicity],
+                "parental level of education": [self.parental_level_of_education],
                 "lunch": [self.lunch],
-                "test_preparation_course": [self.test_preparation_course],
-                "reading_score": [self.reading_score],
-                "writing_score": [self.writing_score],
+                "test preparation course": [self.test_preparation_course],
+                "reading score": [self.reading_score],
+                "writing score": [self.writing_score],
             }
 
             return pd.DataFrame(custom_data_input_dict)
