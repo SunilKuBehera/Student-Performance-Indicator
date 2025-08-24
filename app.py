3dae8546c3a54118dd80ea1f55cfac3476488cec
SunilKuBehera <imsunil050@gmail.com>
2025-08-24 17:16:58 +0530

    Webpage modified

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sys
 import os
 import pandas as pd
 import webbrowser
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 from src.exception import CustomException
 from src.utils import load_object
@@ -14,40 +14,38 @@ app = Flask(__name__, template_folder='templates')
 def home():
     return render_template('home.html', results=None)
 
-@app.route('/predictdata', methods=['GET', 'POST'])
+@app.route('/predictdata', methods=['POST'])
 def predict_datapoint():
-    if request.method == 'GET':
-        return render_template('home.html')
-    else:
-        try:
-            data = CustomData(
-                gender=request.form.get('gender'),
-                race_ethnicity=request.form.get('race_ethnicity'),
-                parental_level_of_education=request.form.get('parental_level_of_education'),
-                lunch=request.form.get('lunch'),
-                test_preparation_course=request.form.get('test_preparation_course'),
-                reading_score=float(request.form.get('reading_score')),
-                writing_score=float(request.form.get('writing_score'))
-            )
+    try:
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('race_ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
+        )
 
-            pred_df = data.get_data_as_data_frame()
-            print("Input DataFrame:\n", pred_df)
+        pred_df = data.get_data_as_data_frame()
+        print("Input DataFrame:\n", pred_df)
 
-            predict_pipeline = PredictPipeline()
-            results = predict_pipeline.predict(pred_df)
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
 
-            return render_template('home.html', results=round(results[0], 2))
+        # Respond with JSON (for Fetch API)
+        return jsonify({
+            "predicted_score": round(results[0], 2),
+            "inputs": request.form.to_dict()
+        })
 
-        except Exception as e:
-            return f"An error occurred: {e}"
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
-    # The URL for your Flask app
     url = "http://127.0.0.1:5000/"
-
-    # Open the URL in a new browser tab only in the main process
     if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
         webbrowser.open_new_tab(url)
 
-    # Run the Flask app
     app.run(host="0.0.0.0", debug=True)
